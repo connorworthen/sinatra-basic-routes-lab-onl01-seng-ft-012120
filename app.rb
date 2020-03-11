32ce6f24e54415a6e367b50522fad4657ce43d05
connor worthen <connorw220@protonmail.com>
2020-03-11 22:04:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Connor"
+    end
+
+    get '/hometown' do
+        "My hometown is Los Gatos"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Best American by Flastbush Zombies"
+    end
 end
